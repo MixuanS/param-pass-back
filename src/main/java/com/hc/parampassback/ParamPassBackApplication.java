69b74d0a4ebb560998a95ffc28156b2fa8a74647
贺超 <hechao@hechaodeMacBook-Pro.local>
2023-01-30 17:33:28 +0800

    项目初始化

---
+++ src/main/java/com/hc/parampassback/ParamPassBackApplication.java
@@ -0,0 +1,13 @@
+package com.hc.parampassback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParamPassBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParamPassBackApplication.class, args);
+    }
+
+}
